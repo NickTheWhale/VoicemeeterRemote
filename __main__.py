aa89e2ce3126547347c7fcea9921ade6634b796b
NickTheWhale <51139062+NickTheWhale@users.noreply.github.com>
2022-04-09 00:53:37 -0500

    speed

--- __main__.py
+++ __main__.py
@@ -8,7 +8,6 @@ kind = 'potato'
 # voicemeeter.launch(kind)
 
 with voicemeeter.remote(kind) as vmr:
-
     def get_ver():
         ver = ''
         for i in range(4):
@@ -18,12 +17,46 @@ with voicemeeter.remote(kind) as vmr:
         return ver
 
     vmr.apply({
-        'in-0': dict(A1=True)
-        'in-1': dict()
-        'in-2': dict()
+        'in-0': dict(A1=True, A2=True, A3=True, A4=True, A5=True, B1=True,
+                     B2=True, B3=True),
+        'in-1': dict(A1=True, A2=True, A3=True, A4=True, A5=True, B1=True,
+                     B2=True, B3=True),
+        'in-2': dict(A1=True, A2=True, A3=True, A4=True, A5=True, B1=True,
+                     B2=True, B3=True),
+        'in-3': dict(A1=True, A2=True, A3=True, A4=True, A5=True, B1=True,
+                     B2=True, B3=True),
+        'in-4': dict(A1=True, A2=True, A3=True, A4=True, A5=True, B1=True,
+                     B2=True, B3=True),
+        'in-5': dict(A1=True, A2=True, A3=True, A4=True, A5=True, B1=True,
+                     B2=True, B3=True),
+        'in-6': dict(A1=True, A2=True, A3=True, A4=True, A5=True, B1=True,
+                     B2=True, B3=True)
     })
 
-    print(get_ver())
+    vmr.apply({
+        'in-0': dict(A1=False, A2=False, A3=False, A4=False, A5=False, B1=False,
+                     B2=False, B3=False),
+        'in-1': dict(A1=False, A2=False, A3=False, A4=False, A5=False, B1=False,
+                     B2=False, B3=False),
+        'in-2': dict(A1=False, A2=False, A3=False, A4=False, A5=False, B1=False,
+                     B2=False, B3=False),
+        'in-3': dict(A1=False, A2=False, A3=False, A4=False, A5=False, B1=False,
+                     B2=False, B3=False),
+        'in-4': dict(A1=False, A2=False, A3=False, A4=False, A5=False, B1=False,
+                     B2=False, B3=False),
+        'in-5': dict(A1=False, A2=False, A3=False, A4=False, A5=False, B1=False,
+                     B2=False, B3=False),
+        'in-6': dict(A1=False, A2=False, A3=False, A4=False, A5=False, B1=False,
+                     B2=False, B3=False)
+    })
 
-    # Resets all UI elements to a base profile
-    # vmr.reset()
+    parameter = ['A1', 'A2', 'A3', 'A4', 'A5', 'B1', 'B2', 'B3', 'Mute', 'Mono', 'Solo']
+
+    for j in range(len(parameter)):
+        for i in range(8):
+            print('Strip[' + str(i) + '].' + parameter[j])
+            vmr.set('Strip[' + str(i) + '].' + parameter[j], 1)
+    for j in range(len(parameter)):
+        for i in range(8):
+            print('Strip[' + str(i) + '].' + parameter[j])
+            vmr.set('Strip[' + str(i) + '].' + parameter[j], 0)
